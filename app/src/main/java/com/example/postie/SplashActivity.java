9cbaaea697ca26bd3e67ed62b9b6cdef6465427c
Abhay Sharma <abhay.official22@gmail.com>
2022-05-05 15:19:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/postie/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.postie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep(2000);
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
